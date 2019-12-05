e3c7958737ce79d4771146c942b69f52353b1a56
Bhavya <btanguturu@eagles.ewu.edu>
2019-12-05 09:49:29 -0800

    created roster app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Airtable from 'airtable';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const base = new Airtable({ apiKey: 'keyNyK2dVB4Da7Dzo' }).base('appp6saR18DlLK5EO');
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      records: []
+    };
+  }
+
+  componentDidMount() {
+    base('Roster').select({view: 'Grid view'})
+    .eachPage(
+      (records, fetchNextPage) => {
+        this.setState({
+          records
+        });
+        console.log(records);
+        fetchNextPage();
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+
+        {this.state.records.length > 0 ? (
+        this.state.records.map((record, index) =>
+          <div key={index}>
+            <h2>{record.fields['Name']}</h2>
+            <p><a href={record.fields['Homepage']}>Homepage</a></p>
+            <img src={record.fields['Attachments']} alt="A selfie"/>
+          </div>
+        )
+        ) : (
+          <p>Loading...</p>
+        )}
+
+
+      </div>
+    );
+  }
 }
 
 export default App;
+
